ebcd60f5336e93bf00581d8ef8177f0e057f4550
Charles Abdel-Nabi <chasnabi@gmail.com>
2015-10-21 10:32:30 -0400

    pair up students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,4 +1,17 @@
 students = %w(Ahmed Tarek abdullah randy adam Prince bethany Ailene Alyssa)
 
-puts students
-puts "#{students}"
+
+
+students.shuffle!
+
+
+
+count = 0
+
+until count == (students.length-1)
+
+	puts "#{students[count]} & #{students[count+1]}"
+
+	count += 2
+
+end
